c5607c8dc66beec660d7f02819785172a2e7d824
Борис Кебаб <levon44432@gmail.com>
2024-05-26 17:46:25 +0300

    Added logging behavior for MediatR

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -4,13 +4,11 @@ namespace Catalog.API.Products.GetProductById;
 public record GetProductsByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
 public record GetProductByIdResult(Product Product);
 
-internal class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductsByIdQuery> logger) 
+internal class GetProductByIdQueryHandler(IDocumentSession session) 
     : IRequestHandler<GetProductsByIdQuery, GetProductByIdResult>
 {
     public async Task<GetProductByIdResult> Handle(GetProductsByIdQuery query, CancellationToken cancellationToken)
     {
-        logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
-
         var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
 
         if(product is null)
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -5,6 +5,7 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
 builder.Services.AddValidatorsFromAssembly(assembly);
 
@@ -14,6 +15,10 @@ builder.Services.AddMarten(options =>
 {
     options.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
+
+//Seed db if in dev environment
+if (builder.Environment.IsDevelopment())
+    builder.Services.InitializeMartenWith<CatalogInitialData>();
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
